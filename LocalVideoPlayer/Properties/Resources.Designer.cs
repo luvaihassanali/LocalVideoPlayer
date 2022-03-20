e8f4a813745bb49f9add8e80f4df37bdfaa55e9d
Luvai Hassanali <luvaihassanali@gmail.com>
2022-03-20 05:21:58 -0500

    update play icon

--- LocalVideoPlayer/Properties/Resources.Designer.cs
+++ LocalVideoPlayer/Properties/Resources.Designer.cs
@@ -202,16 +202,6 @@ namespace LocalVideoPlayer.Properties {
         /// <summary>
         ///   Looks up a localized resource of type System.Drawing.Bitmap.
         /// </summary>
-        internal static System.Drawing.Bitmap pause {
-            get {
-                object obj = ResourceManager.GetObject("pause", resourceCulture);
-                return ((System.Drawing.Bitmap)(obj));
-            }
-        }
-        
-        /// <summary>
-        ///   Looks up a localized resource of type System.Drawing.Bitmap.
-        /// </summary>
         internal static System.Drawing.Bitmap pause64 {
             get {
                 object obj = ResourceManager.GetObject("pause64", resourceCulture);
